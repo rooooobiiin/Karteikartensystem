46db5618bc2ed07f6597f468f2407ee3a678603c
AminAsli <amin-asli@hotmail.com>
2022-04-21 23:47:13 +0200

    Scenes

--- src/main/java/de/atiw/karteikartensystem/UI/WelcomeController.java
+++ src/main/java/de/atiw/karteikartensystem/UI/WelcomeController.java
@@ -15,7 +15,7 @@ public class WelcomeController {
     @FXML
     public void login(ActionEvent actionEvent) {
 //        SystemController.connectToDB(user.getText(), password.getText());
-        GUIController.switchScene(SceneName.Welcome);
+        GUIController.switchScene(SceneName.Edit);
 
         // TODO: Überprüfe, wenn nicht Fehlermeldung und nochmal veruschen
         // TODO: AMIN: --> Brauchen Textfeld für Fehlermeldung
