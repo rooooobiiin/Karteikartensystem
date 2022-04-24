db0dd8440f9cba52ddfb6319e28a907f7c35b7ec
robinschiller <robin-schiller@web.de>
2022-04-24 21:09:29 +0200

    logik neu //

--- src/main/java/de/atiw/karteikartensystem/UI/Controller/SuccessfulControler.java
+++ src/main/java/de/atiw/karteikartensystem/UI/Controller/SuccessfulControler.java
@@ -3,11 +3,13 @@ package de.atiw.karteikartensystem.UI.Controller;
 import de.atiw.karteikartensystem.UI.GUIController;
 import de.atiw.karteikartensystem.UI.SceneName;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -34,10 +36,10 @@ public class SuccessfulControler implements Initializable{
     }
 
 
-    public void keypressed(KeyEvent keyEvent) {
-//        String character = keyEvent.getCharacter();
-//        GUIController.message = character;
-//        GUIController.switchScene(SceneName.Successful);
-
+    public void keypressed (KeyEvent k){
+            if (k.getCode().equals(KeyCode.ENTER)) {
+                System.out.println("HALLO");
+            }
     }
+
 }
