c4acc37cd3c7696c49eebf96bf9c19f47b660279
Rico <rico.zimmermann@hotmail.de>
2022-04-24 21:03:11 +0200

    Scenes

--- src/main/java/de/atiw/karteikartensystem/UI/Controller/SuccessfulControler.java
+++ src/main/java/de/atiw/karteikartensystem/UI/Controller/SuccessfulControler.java
@@ -5,6 +5,7 @@ import de.atiw.karteikartensystem.UI.SceneName;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 
@@ -15,10 +16,13 @@ public class SuccessfulControler implements Initializable{
 
     @FXML
     private Label message_box;
+    @FXML
+    private Button btn_ok;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         message_box.setText(GUIController.message);
+
         
     }
 
